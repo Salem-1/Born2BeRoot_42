accfe2286521679532b21884e846270fb2624735
Ahmed Salem <ahsalem@lab1r3s10.42abudhabi.ae>
2022-06-12 17:29:18 +0400

    waiting for the monitoring.sh to submit

--- docs_b2br.c
+++ docs_b2br.c
@@ -197,12 +197,28 @@ Defaults	logfile="/var/log/sudo/sudo.log"
 Defaults	log_input,log_output
 Defaults        requiretty
 Defaults   secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+"
 $ hostnamectl
 $ hostnamectl set-hostname new_hostname
 $ sudo nano /etc/hosts
 127.0.0.1       localhost
 127.0.0.1       new_hostname
  sudo reboot
+//configuring the crontab
  $ sudo crontab -u root -e
  */10 * * * * /usr/local/bin/monitoring.sh
-  drm:vmw_host_log *ERROR* Failed to send host log message.
+
+  monitoring.sh script draft
+  #!/bin/bash
+wall $'#Architecture: ' `hostnamectl | grep "Operating System" | cut -d ' ' -f5- ` `awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'` `arch` \
+$'\n#CPU physical: '`cat /proc/cpuinfo | grep processor | wc -l` \
+$'\n#vCPU:  '`cat /proc/cpuinfo | grep processor | wc -l` \
+$'\n'`free -m | awk 'NR==2{printf "#Memory Usage: %s/%sMB (%.2f%%)", $3,$2,$3*100/$2 }'` \
+$'\n'`df -h | awk '$NF=="/"{printf "#Disk Usage: %d/%dGB (%s)", $3,$2,$5}'` \
+$'\n'`top -bn1 | grep load | awk '{printf "#CPU Load: %.2f\n", $(NF-2)}'` \
+$'\n#Last boot: ' `who -b | awk '{print $3" "$4" "$5}'` \
+$'\n#LVM use: ' `lsblk |grep lvm | awk '{if ($1) {print "yes";exit;} else {print "no"} }'` \
+$'\n#Connection TCP:' `netstat -an | grep ESTABLISHED |  wc -l` \
+$'\n#User log: ' `who | cut -d " " -f 1 | sort -u | wc -l` \
+$'\nNetwork: IP ' `hostname -I`"("`ip a | grep link/ether | awk '{print $2}'`")" \
+$'\n#Sudo:  ' `grep 'sudo ' /var/log/auth.log | wc -l`
